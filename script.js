bceb9514f02658b1e96de373d953c5bc11d6d43b
rb-hv <kieranprovost+robohive@gmail.com>
2025-02-27 18:52:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var alertTimes = 0;
+if (alertTimes == 0) {
+  setTimeout(function() {
+    alert("You have reached Saline RoboHive's public website! Click on different pages to learn more.");
+    alertTimes = 1;
+  }, 5000);
+}
