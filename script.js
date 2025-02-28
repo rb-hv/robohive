70f5314cd52ba296968cf061217445d80f86ba6a
rb-hv <kieranprovost+robohive@gmail.com>
2025-02-27 21:06:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('play').addEventListener('click', function() {
+      var audioElement = document.getElementById('audioElement');
+      audioElement.style.display = 'block';
+      audioElement.play();
+  });
 
-setTimeout(function() {
-  alert("You have reached Saline RoboHive's public website! Click on different pages to learn more.");
-}, 5000);
-
+  setTimeout(function() {
+      alert("You have reached Saline RoboHive's public website! Click on different pages to learn more.");
+  }, 5000);
+});
